68b2f9bbe2b3bd614fb2d21be8b58ad4cccca190
Alexi GRIGOROV <alexigrigorov@gmail.com>
2024-08-04 08:17:00 +0200

    now the last github raw url change

--- src/scripts/models/DataModelPhotographer.mjs
+++ src/scripts/models/DataModelPhotographer.mjs
@@ -1,7 +1,7 @@
 export default class DataModelPhotographer {
   constructor(rawData) {
     this.id = rawData.id;
-    this.portrait = `https://raw.githubusercontent.com/alexiGrigorov/OCPR05-FishEye/assets/images/photographers/thumbnails/${rawData.portrait}`;
+    this.portrait = `https://raw.githubusercontent.com/alexiGrigorov/OCPR05-FishEye/main/assets/images/photographers/thumbnails/${rawData.portrait}`;
     this.name = rawData.name;
     this.location = `${rawData.city}, ${rawData.country}`;
     this.tagline = rawData.tagline;
